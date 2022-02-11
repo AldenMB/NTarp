40f695eb3120e40c630c0dc64b20baf415885777
Alden Bradford <aldenbradford@gmail.com>
2022-02-11 11:29:00 -0500

    improve docs, remove inappropriate mixin

--- NTarp.py
+++ NTarp.py
@@ -6,7 +6,50 @@ from sklearn.random_projection import GaussianRandomProjection
 from sklearn.exceptions import DataDimensionalityWarning
 
 
-class NTarp(TransformerMixin, ClusterMixin, BaseEstimator):
+class NTarp(ClusterMixin, BaseEstimator):
+    """
+    This is an implementation of the nTARP clustering method.
+    This method selects a hyperplane $X\cdot direction = threshold$ 
+    to split the data set.
+    
+    Parameters
+    ----------
+    n : int, default=50
+        the parameter n from the nTARP method, which indicates
+        how many trial projections to perform. 50 is a good value
+        for many cases, though some may require a higher value.
+        This is essentially a fitting parameter; increasing n will
+        lead to higher-quality clusterings and higher runtimes, but
+        will also increase the chance of overfitting.
+        
+    random_state : int, RandomState instance or None, default=None
+        Controls the pseudo random number generator used to generate the matrix
+        at fit time.
+        Pass an int for reproducible output across multiple function calls.
+        See :term:`Glossary <random_state>`.
+        
+    Attributes
+    ----------
+    direction_ : ndarray of shape (X.shape[-1],)
+        the direction used in defining the separating hyperplane.
+        
+    threshold_ : float
+        the threshold used in definint the separating hyperplane.
+        
+    Notes
+    -----
+    50 is a good value of n for many cases, though some may require a
+    higher value. This is essentially a fitting parameter; increasing
+    n will lead to higher-quality clusterings and higher runtimes, 
+    but will also increase the chance of overfitting.
+    
+    References
+    ----------
+    [1] https://arxiv.org/pdf/1806.05297.pdf the original
+    description of the method
+    [2] https://arxiv.org/abs/2008.09579 a more theoretical
+    development of its structure.
+    """
     def __init__(self, n=50, *, random_state=None):
         self.n = n
         self.projector = GaussianRandomProjection(n, random_state=random_state)
@@ -15,12 +58,31 @@ class NTarp(TransformerMixin, ClusterMixin, BaseEstimator):
         self.labels_ = None
 
     def fit(self, X, y=None):
+        """ Compute nTARP clustering.
+        
+        
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            Training instances to cluster.
+        y : Ignored
+            Not used, present here for API consistency by convention.
+            
+        """
+        # We are not using the projector to reduce to dimension n, 
+        # but rather to one dimension n times. These are numerically
+        # equivalent, but it means the projector will give an
+        # inappropriate warning if n_features is less than n.
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=DataDimensionalityWarning)
             self.projector.fit(X)
         return self.refit(X)
 
     def refit(self, X, y=None):
+        """ Re-choose a direction and threshold based on
+        the same directions, but using new data. This can be used
+        to validate the choice of direction and threshold.
+        """
         projection = self.projector.transform(X)
         direction_index = w(projection.T).argmin()
         self.direction_ = self.projector.components_[direction_index]
@@ -30,35 +92,65 @@ class NTarp(TransformerMixin, ClusterMixin, BaseEstimator):
         return self
 
     def predict(self, X):
+        """Predict the cluster assignment of each sample in X.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            New data to predict.
+
+        Returns
+        -------
+        labels : ndarray of shape (n_samples,)
+            boolean indicating each sample's cluster assignment.
+        """
         return np.inner(X, self.direction_) > self.threshold_
 
     def score(self, X):
+        """
+        By convention .score should be bigger-is-better, while
+        nTARP works by minimizing the parameter w. Hence its complement
+        1-w is a good choice for score, since it is always positive.
+        This is analogous to the R^2 value used in regression, in that
+        it also represents a fraction of explained variance.
+        """
         return 1 - w(self.predict(X))
 
     def gaussian_p(self, X):
+        """
+        Under the assumption that X was drawn from a Gaussian 
+        distribution, this gives the p-value of the observed w score
+        based on a limiting argument given in [2].
+        """
         alternative_w = w(self.predict(X))
         null_model = distribution_of_w_assuming_gaussian_data(X.shape[-1])
         return null_model.cdf(alternative_w)
 
 
-def w(x, axis=-1, threshold=None):
+def w(X, threshold=None):
     """
-    Compute the fraction of variance not explained by a simple thresholding.
-    x is a numpy array holding the data on which W should be computed.
-    axis only works with its default value currently.
-    threshold should only be used when x is flat.
+    The fraction of variance not explained by a simple thresholding,
+    which coincides with the WithinSS clustering index.
+    
+    Parameters
+    ----------
+    X : ndarray of shape (n_samples,) or (n_trials, n_samples)
+    
+    threshold : optional, float or ndarray of shape (n_trials)
+        The threshold at which to compute w. If not provided,
+        the best threshold is chosen for the given X.
     """
-    sorted_x = np.sort(x, axis=axis)
-    varlist = explained_variance_list(sorted_x, axis=axis)
+    sorted_x = np.sort(X, axis=-1)
+    varlist = explained_variance_list(sorted_x, axis=-1)
     if threshold is None:
-        best_variance = varlist.max(axis=axis)
+        best_variance = varlist.max(axis=-1)
     else:
         below_threshold_count = (sorted_x < threshold).sum()
         if below_threshold_count in (0, len(sorted_x)):
             best_variance = 0
         else:
             best_variance = varlist[below_threshold_count - 1]
-    return 1 - best_variance / sorted_x.var(axis=axis)
+    return 1 - best_variance / sorted_x.var(axis=-1)
 
 
 def explained_variance_list(x, axis=-1):
@@ -108,9 +200,7 @@ def distribution_of_w_assuming_gaussian_data(n):
         )
     if n < 10:
         warnings.warn(
-            "calling function distribution_of_w_assuming_gaussian_data with n={}. This provides a poor approximation when n<10.".format(
-                n
-            )
+            f"calling function distribution_of_w_assuming_gaussian_data with n={n}. This provides a poor approximation when n<10."
         )
     k2 = (8 * (np.pi - 3)) / np.pi**2
     s2 = 1 - (2 / np.pi)
